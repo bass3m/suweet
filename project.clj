aaee6fe7c28cf5184cc7c19ac3fcebca198b123c
Bassem Youssef <bassem@outlook.com>
2013-06-01 14:36:37 -0700

    Add main and fix slf4j error that mysteriously appeared.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :main twum.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.slf4j/slf4j-simple "1.6.2"]
                  [twitter-api "0.7.4"]])
