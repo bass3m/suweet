44dbfb6c08c4bf4fcc54bd2353dbb181769bc0a3
Bassem Youssef <bassem@outlook.com>
2013-06-07 23:58:25 -0700

    Looks to be working with sorting.

--- src/twum/score.clj
+++ src/twum/score.clj
@@ -13,11 +13,22 @@
   follows (3 * favorite-count + retweet-count) / follower-count"
   [fav rt fol]
   (/ (+ rt (* 3 fav)) fol))
+  ;(with-precision 3 (/ (+ rt (* 3M fav)) fol))  ; perhaps
 
+(defn default-sort-fn
+  "Given an item in the twlist coll, return a sortable value."
+  ([link] (default-sort-fn link default-score-fn))
+  ([link score-fn] (apply score-fn ((juxt :rt-counts :fav-counts :follow-count) link))))
+
+; (def my-tw (read-tw-lists cfg))
+; (def tech-tw (last my-tw))
+; use sort-by function perhaps
+; (map #((juxt :rt-counts :fav-counts :follow-count) %) (:links tech-tw))
+; (map (fn [[n d]] (with-precision 3 (/ n d))) [[7 24088M]  [123 1024000M]])
 (defn top-list-tweets
   "Process twitter list and return the top tweets"
-  ([tw-list] (top-list-tweets tw-list 10 default-score-fn))
-  ([tw-list top-n score-fn] ()))
+  ([tw-list] (top-list-tweets tw-list 10 default-sort-fn))
+  ([tw-list top-n sort-fn] (take top-n (sort-by sort-fn (:links tw-list)))))
 
 (defn top-tweets
   "Process all of our lists for the top tweets in each list.
@@ -25,7 +36,7 @@
   [cfg-file]
   ; read config file
   (map top-list-tweets (-> {:cfg-file cfg-file} read-cfg read-tw-lists))
-  (read-tw-lists (:directory (read-cfg {:cfg-file cfg-file})))
+  ;(read-tw-lists (:directory (read-cfg {:cfg-file cfg-file})))
   )
 
 ;; invoke (top-tweets "config.txt")
