0886eaf6a3e8d5b253ce11cc1a5dcab124628626
Bassem Youssef <bassem@outlook.com>
2013-06-09 00:31:20 -0700

    Improve format to get handle tweets with no urls better.

--- src/twum/score.clj
+++ src/twum/score.clj
@@ -1,4 +1,5 @@
 (ns twum.score
+  (:use [clojure.string :only (join)])
   (:use [twum.core :only (read-cfg read-tw-lists)]))
 
 ; read config file, to find where we keep tweets
@@ -6,7 +7,16 @@
 ;
 ; How we assign tweet scores:
 ; score = (3 * fav + rt) / num of followers from tweet
+; display by 'map'ping over print
 
+(defn format-top-tweet
+  "Format a top tweet in a more user friendly view"
+  [tw]
+  (format "\nBy: %s\n%s\n%sReTweeted by: %d Favorited by: %d Followers: %d\n"
+          (join ":" (concat (:urlers tw)))
+          (join ":" (concat (:text tw)))
+          (if (not (nil? (:url tw))) (join ["Link: " (:url tw) "\n"]) "") 
+          (:rt-counts tw) (:fav-counts tw) (:follow-count tw)))
 
 (defn default-score-fn
   "The default scoring function for a tweet. We calculate the score as
@@ -28,16 +38,12 @@
 (defn top-list-tweets
   "Process twitter list and return the top tweets"
   ([tw-list] (top-list-tweets tw-list 10 default-sort-fn))
-  ([tw-list top-n sort-fn] (take top-n (sort-by sort-fn (:links tw-list)))))
+  ([tw-list top-n sort-fn] (map format-top-tweet
+                                (take top-n (reverse (sort-by sort-fn (:links tw-list)))))))
 
+;; invoke (top-tweets "config.txt")
 (defn top-tweets
   "Process all of our lists for the top tweets in each list.
   We're given the location of the config file"
   [cfg-file]
-  ; read config file
-  (map top-list-tweets (-> {:cfg-file cfg-file} read-cfg read-tw-lists))
-  ;(read-tw-lists (:directory (read-cfg {:cfg-file cfg-file})))
-  )
-
-;; invoke (top-tweets "config.txt")
-
+  (map top-list-tweets (-> {:cfg-file cfg-file} read-cfg read-tw-lists)))
