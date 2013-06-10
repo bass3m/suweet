20e9d0bc670c356ef3ea04328c33011375e59beb
Bassem Youssef <bassem@outlook.com>
2013-06-10 15:48:16 -0700

    Start on summarization.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject twum "0.1.0-SNAPSHOT"
-  :description "Twitter utility for summarizing lists"
+  :description "Utility for summarizing Twitter lists"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,4 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.slf4j/slf4j-simple "1.6.2"]
                  [clj-time "0.5.1"]
-                 [twitter-api "0.7.4"]])
+                 [twitter-api "0.7.4"]
+                 [org.apache.opennlp/opennlp-tools "1.5.3"]
+                 [org.clojars.gnarmis/snowball-stemmer "0.1.1-SNAPSHOT"]
+                 [clojure-opennlp "0.3.0"]])
