498e3f866862d1b3c185da03f2ac8711f91613dc
Bassem Youssef <bassem@outlook.com>
2013-06-12 09:18:06 -0700

    Refactor a little by adding multiple dispatch for the potential to use multiple summarizing algorithms.

--- src/twum/summarize.clj
+++ src/twum/summarize.clj
@@ -11,19 +11,25 @@
 ; stemmer see: http://snowball.tartarus.org/
 (def en-stemmer     (stemmer "english"))
 
+(defmulti rank-sentence 
+  (fn [sentence significant-words cfg] 
+    (get-in cfg [:algorithm :type])))
+
 ; find max score over the sentence within the cluster size.
-; very basic ranking algorithm
-(defn rank-sentence
-  ([sentence significant cfg] (rank-sentence sentence (map key significant) cfg 0))
-  ([sentence significant cfg score]
+; very basic and probably dumb ranking algorithm
+(defmethod rank-sentence :basic
+  ([sentence significant cfg] 
+   (rank-sentence sentence (map key significant) 
+                  (get-in cfg [:algorithm :params :word-cluster-size]) 0))
+  ([sentence significant word-cluster-size score]
    (if (empty? sentence)
      score
-     (recur (rest sentence) significant cfg
+     (recur (rest sentence) significant word-cluster-size
                   (max score
                        (reduce (fn [acc x]
                                  (if (some #(= x %) significant)
                                              (inc acc) acc))
-                               0 (take (:word-cluster-size cfg) sentence)))))))
+                               0 (take word-cluster-size sentence)))))))
 
 (defn filter-sentence
   "Given a sentence return tokenized sentence together with the
@@ -34,31 +40,38 @@
         sig significant
         :when (= sig (second snt))] snt))
 
-; (def sec-snt (second (map #(filter-sentence % (map key sigy)) sents)))
-(defn sentence-rank
-  "Calculate the sentence score. Find the maximum score for a segment
-  of a sentence within :word-cluster-size words. We are passed words
-  which are significant together with their location in the sentence.
-  The score is calculated by squaring the num of significant word count
-  divided by the total num of words in that segment.
-  sentence: tokenized words in the sentence.
-  sigificant: stemmed significant words along with their frequencies."
+;; Calculate the sentence score. Find the maximum score for a segment
+;; of a sentence within :word-cluster-size words. We are passed words
+;; which are significant together with their location in the sentence.
+;; The score is calculated by squaring the num of significant word count
+;; divided by the total num of words in that segment.
+;; sentence: tokenized words in the sentence.
+;; sigificant: stemmed significant words along with their frequencies. 
+(defmethod rank-sentence :luhn
   [sentence significant cfg]
-  (let [sentence (filter-sentence sentence (map key significant))]
+  (let [sentence (filter-sentence sentence (map key significant))
+        word-cluster-size (get-in cfg [:algorithm :params :word-cluster-size])]
     (:max-score 
       (reduce (fn [acc [n _]] 
-                    (cond
-                      (or (> n (+ (:word-cluster-size cfg) (:last acc)))
-                          (zero? (:sig-size acc))) (assoc acc :start n :sig-size 1 :last n 
-                                                          :max-score (max (:max-score acc) 1))
-                      :else (let [new-sig (inc (:sig-size acc))
-                                  size (+ 1 (- n (:start acc)))] 
-                              (-> acc (assoc :sig-size new-sig)
-                                  (assoc :max-score (max (:max-score acc)
-                                                         (/ (* new-sig new-sig)
-                                                            size)))
-                                  (assoc :last n)))))
-                  {:last 0 :start 0 :sig-size 0 :max-score 0} sentence))))
+                (cond
+                  (or (> n (+ word-cluster-size (:last acc)))
+                      (zero? (:sig-size acc))) 
+                    (assoc acc :start n 
+                         :sig-size 1 :last n 
+                         :max-score (max (:max-score acc) 1))
+                  :else (let [new-sig (inc (:sig-size acc))
+                              size (+ 1 (- n (:start acc)))] 
+                          (-> acc (assoc :sig-size new-sig)
+                              (assoc :max-score (max (:max-score acc)
+                                                     (/ (* new-sig new-sig)
+                                                        size)))
+                              (assoc :last n)))))
+              {:last 0 :start 0 :sig-size 0 :max-score 0} sentence))))
+
+(defmethod rank-sentence :default
+  [sentence significant cfg]
+  (do (println "Not implemented yet: Sentence" sentence)
+      (println "Config: " cfg)))
 
 (defn filter-non-words
   "Given a coll of strings, get rid of non-words, periods, commas for example."
@@ -96,8 +109,7 @@
 (defn summarize
   "Attempt to summarize a given text into it's most relevant sentences"
   ([text] (summarize text {:num-sentences 3
-                           :algorithm {:type "luhn" :params {:word-cluster-size 4}}
-                           :word-cluster-size 4
+                           :algorithm {:type :luhn :params {:word-cluster-size 4}}
                            :top-n-pct 0.9 ; XXX should be a freq distrib
                            :stop-words "models/english.txt"}))
   ([text cfg]
@@ -111,19 +123,9 @@
                          clojure.string/lower-case
                          tokenize
                          stem-words ;; added
-                         (sentence-rank significant cfg)))
+                         (rank-sentence significant cfg)))
           sentences-map))))
-
-
-
-;(take (:num-sentences cfg) (reverse (sort-by )))
-
+;
+;(take (:num-sentences cfg) (reverse (sort-by :score (summarize text)))
+; then sort by actual order.
 ;; (take (Math/round (* 8 0.9)) (significant-words senty {:stop-words "models/english.txt"}))
-;; Process words: find all words:
-;; lowercase all , filter through stop-words,
-;; use stemmer,
-;; count words and sort (generate a freq dist) (take top 100 words)
-;; this will provide the significant words
-;; find sentences: lowercase as well
-;; find sentence score.
-;;
