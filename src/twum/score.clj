43e8a7e64ca8a6c09419d35a4beb4896b4a595da
Bassem Youssef <bassem@outlook.com>
2013-06-14 20:34:42 -0700

    Refactor score and sort config and allow them to be configurable. Also add option to control the number of top tweets returned.

--- src/twum/score.clj
+++ src/twum/score.clj
@@ -8,30 +8,45 @@
   (format "\nBy: %s\n%s\n%sReTweeted by: %d Favorited by: %d Followers: %d\n"
           (s/join ":" (concat (:urlers tw)))
           (s/join ":" (concat (:text tw)))
-          (if (not (nil? (:url tw))) (s/join ["Link: " (:url tw) "\n"]) "") 
+          (if (not (nil? (:url tw))) (s/join ["Link: " (:url tw) "\n"]) "")
           (:rt-counts tw) (:fav-counts tw) (:follow-count tw)))
 
-(defn default-score-fn
-  "The default scoring function for a tweet. We calculate the score as
-  follows (3 * favorite-count + retweet-count) / follower-count"
-  [fav rt fol]
+(defmulti score-tweet
+  "Scoring function for a tweet. The idea is to be able to vary the
+  method used to calculate the score of a tweet"
+  (fn [cfg &]
+    (:tw-score cfg)))
+
+;; The default scoring function for a tweet. We calculate the score as
+;; follows (3 * favorite-count + retweet-count) / follower-count"
+(defmethod score-tweet :default
+  [_ fav rt fol]
   (/ (+ rt (* 3 fav)) fol))
 
-(defn default-sort-fn
-  "Given an item in the twlist coll, return a sortable value."
-  ([link] (default-sort-fn link default-score-fn))
-  ([link score-fn] (apply score-fn ((juxt :rt-counts :fav-counts :follow-count) link))))
+;; Define method for sorting the tweets
+(defmulti sort-tweet
+  (fn [cfg _]
+    (:tw-sort cfg)))
+
+;; the default option is to use a calculation based on
+;; favorite counts, retweet counts and the number of followers
+(defmethod sort-tweet :default
+  [cfg link]
+  (apply score-tweet cfg ((juxt :fav-counts :rt-counts :follow-count) link)))
 
 (defn top-list-tweets
   "Process twitter list and return the top tweets"
-  ([tw-list] (top-list-tweets tw-list 10 default-sort-fn))
-  ([tw-list top-n sort-fn] (map format-top-tweet
-                                (take top-n (reverse (sort-by sort-fn (:links tw-list)))))))
+  ([tw-list] (top-list-tweets {:top-tweets 10
+                               :tw-sort :default
+                               :tw-score :default} tw-list))
+  ([cfg tw-list] (map format-top-tweet
+                      (take (:top-tweets cfg) 
+                            (reverse (sort-by (partial sort-tweet cfg) (:links tw-list)))))))
 
 (defn top-tweets
   "Process all of our lists for the top tweets in each list.
   We're given the location of the config file"
   [cfg-file]
-  (map top-list-tweets (-> {:cfg-file cfg-file}
-                           twum/merge-cfg
-                           twum/read-tw-lists)))
+  (map (partial top-list-tweets cfg-file) (-> {:cfg-file cfg-file}
+                                              twum/merge-cfg
+                                              twum/read-tw-lists)))
